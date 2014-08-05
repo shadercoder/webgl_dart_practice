f9a4690e52679bd53317896530a8eba60523f382
Peter <shadercoder@gmail.com>
2014-08-05 12:05:04 +0800

    add shader source

--- webgl_practice/web/1/webgl_1.dart
+++ webgl_practice/web/1/webgl_1.dart
@@ -16,6 +16,9 @@ int vpWidth = 0;
 int vpHeight = 0;
 WebGL.RenderingContext _glContext;
 
+WebGL.Buffer triVertexBuffer;
+WebGL.Buffer quadVertexBuffer;
+
 void init()
 {
   glCanvas = querySelector("#drawArea");
@@ -28,8 +31,76 @@ void init()
   
   
   
+  
+  
 }
 
+void createVetexBuffer()
+{
+  // create triangle & quad vertex buffr
+  triVertexBuffer = _glContext.createBuffer();
+  
+  quadVertexBuffer = _glContext.createBuffer();
+  
+  
+  // filling triangle data to vertex buffer
+  List<double> vertices;
+  _glContext.bindBuffer(WebGL.RenderingContext.ARRAY_BUFFER, triVertexBuffer);
+  
+  // real vertex data (include only position)
+  vertices =
+  [
+   0.0, 1.0, 0.0,
+   -1.0,-1.0, 0.0,
+   1.0, -1.0, 0.0
+  ];
+  
+  // fill data to allocated buffer in gpu memory.
+  _glContext.bufferDataTyped(WebGL.RenderingContext.ARRAY_BUFFER, new Float32List.fromList(vertices), WebGL.RenderingContext.STATIC_DRAW);
+  
+  
+}
+
+
+void createShader()
+{
+  WebGL.Shader vertexShader = _glContext.createShader(WebGL.RenderingContext.VERTEX_SHADER);
+  WebGL.Shader fragmentShader = _glContext.createShader(WebGL.RenderingContext.FRAGMENT_SHADER);
+  
+  // vertex shader source..
+  String vs = """
+  attribute vec3 position;
+  
+  uniform mat4 mvMatrix;
+  uniform mat4 projMatrix;
+  
+  void main()
+  { 
+      gl_position = projMatrix * mvMatrix * vec4(position, 1.0);
+  }
+
+  """;
+  
+  String ps = """
+  precision mediump float;
+  
+  void main()
+  {
+     gl_FragColor = vec4(1.0, 1.0, 0.0, 0.0);
+  }
+  """;
+  
+  
+  // compile shaders
+  
+  _glContext.shaderSource(vertexShader, vs);
+  _glContext.compileShader(vertexShader);
+  
+  _glContext.shaderSource(fragmentShader, ps);
+  _glContext.compileShader(fragmentShader);
+  
+  
+}
 void main()
 {
   init();
